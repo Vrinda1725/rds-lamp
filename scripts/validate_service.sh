b489eb2cc3aa2b134942cece1a5b01f9928f4bef
Vrinda1725 <97523662+Vrinda1725@users.noreply.github.com>
2024-12-19 17:22:50 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-curl -f http://localhost || exit 1
+echo "Running ValidateService script for WordPress"
+
+# Check if Apache2 is running
+if systemctl is-active --quiet apache2; then
+  echo "Apache2 is running"
+else
+  echo "Apache2 is not running" >&2
+  exit 1
+fi
+
+# Check WordPress accessibility
+curl -I http://localhost | grep "200 OK" > /dev/null
+if [ $? -eq 0 ]; then
+  echo "WordPress is accessible"
+else
+  echo "WordPress validation failed" >&2
+  exit 1
+fi
