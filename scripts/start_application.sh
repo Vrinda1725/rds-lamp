51a82975e421932c1f8101cf0a226fa90d775c7a
Vrinda1725 <97523662+Vrinda1725@users.noreply.github.com>
2024-12-19 17:22:18 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "Starting Apache..."
+echo "Running ApplicationStart script for WordPress"
+
+# Start Apache service
 sudo systemctl start apache2
+
+# Enable Apache to start on boot
+sudo systemctl enable apache2
+
+# Reload Apache to apply changes
+sudo systemctl reload apache2
