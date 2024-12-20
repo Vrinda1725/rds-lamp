fa452edc93cad74c8925bb5716b07ad21ab18bc8
Vrinda1725 <97523662+Vrinda1725@users.noreply.github.com>
2024-12-20 14:45:17 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,8 +7,4 @@ sudo apt update -y
 # Stop Apache service
 sudo systemctl stop apache2
 
-# Backup existing WordPress installation (optional)
-if [ -d "/var/www" ]; then
-  sudo tar -czf /var/www_backup_$(date +%F-%T).tar.gz /var/www
-  sudo rm -rf /var/www/*
-fi
+
